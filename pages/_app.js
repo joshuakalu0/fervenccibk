e767123722a36c528d67d91b8b30fe50f62c7ca2
joshuakalu0 <ajujoshua2019@gmail.com>
2023-07-25 15:50:07 +0100

    NB

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react";
+import "../styles/globals.css";
+import { createContext, useState } from "react";
+import { useRouter } from "next/router";
+import ImageModel from "../components/utiles_fun/ImageModel";
+// import Model from "../components/dashboard/Model";
+import Navbar from "../components/utiles/Navbar";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export const Dashboardcontext = createContext();
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  const [maindata, setmaindata] = useState({
+    user: [],
+    skills: [],
+    project: [],
+    report: [],
+    isSideActive: true,
+    displayImages: null,
+  });
+
+  function setdisplayImages(value) {
+    setmaindata({
+      ...maindata,
+      displayImages: value,
+    });
+  }
+
+  return (
+    <SessionProvider session={session}>
+      <Dashboardcontext.Provider value={[maindata, setmaindata]}>
+        <Navbar />
+        {maindata.displayImages && (
+          <ImageModel data={maindata.displayImages} fun={setdisplayImages} />
+        )}
+        {/* <Model data={maindata} setter={setmaindata} /> */}
+        <Component {...pageProps} />
+      </Dashboardcontext.Provider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
