720bceec0648f5364cace2933ced6e9927cd7254
joshuakalu0 <ajujoshua2019@gmail.com>
2023-07-27 05:01:33 +0100

    api change

--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,6 @@
-import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
-import { createContext, useState } from "react";
-import { useRouter } from "next/router";
-import ImageModel from "../components/utiles_fun/ImageModel";
-// import Model from "../components/dashboard/Model";
-import Navbar from "../components/utiles/Navbar";
 
-export const Dashboardcontext = createContext();
-
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  const [maindata, setmaindata] = useState({
-    user: [],
-    skills: [],
-    project: [],
-    report: [],
-    isSideActive: true,
-    displayImages: null,
-  });
-
-  function setdisplayImages(value) {
-    setmaindata({
-      ...maindata,
-      displayImages: value,
-    });
-  }
-
-  return (
-    <SessionProvider session={session}>
-      <Dashboardcontext.Provider value={[maindata, setmaindata]}>
-        <Navbar />
-        {maindata.displayImages && (
-          <ImageModel data={maindata.displayImages} fun={setdisplayImages} />
-        )}
-        {/* <Model data={maindata} setter={setmaindata} /> */}
-        <Component {...pageProps} />
-      </Dashboardcontext.Provider>
-    </SessionProvider>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
 export default MyApp;
